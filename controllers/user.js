a527b29e33a71b4586d43ccfc9fa22cfeb3ac4fc
SangamSilwal <sangamsilwal2062@gmail.com>
2025-03-21 16:12:32 +0545

    jwt

--- controllers/user.js
+++ controllers/user.js
@@ -23,9 +23,9 @@ async function handleUserLogin(req,res)
 
     if(!user) return res.render('login',{error: "Invalid username or password"});
     
-    const sessionId = uuidv4();
-    setUser(sessionId,user);
-    res.cookie('uid',sessionId)
+    
+    const token = setUser(user);
+    res.cookie('uid',token)
 
 
 
--- service/auth.js
+++ service/auth.js
@@ -1,14 +1,19 @@
-const sessionIdToUserMap = new Map();
+const jwt = require('jsonwebtoken')
+const secret = "heySangam";
 
-function setUser(id,user)
+function setUser(user)
 {
-    sessionIdToUserMap.set(id,user)
+    return jwt.sign({
+        _id:user._id,
+        email: user.email,
+    },secret)
 
 }
 
-function getUser(id)
+function getUser(token)
 {
-    return sessionIdToUserMap.get(id);
+    if(!token) return null
+    return jwt.verify(token,secret)
 }
 
 module.exports = {
